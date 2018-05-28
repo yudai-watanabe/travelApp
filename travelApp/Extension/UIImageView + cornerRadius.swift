b2b190f9031b11ef2f0d08d6cf8155c882753e96
yudai watanabe <judas.wata@gmail.com>
2018-05-28 14:35:42 +0900

    update cardUI

---
+++ travelApp/Extension/UIImageView + cornerRadius.swift
@@ -0,0 +1,27 @@
+//
+//  UIImageView + cornerRadius.swift
+//  travelApp
+//
+//  Created by watanabe-yudai on 2018/05/28.
+//  Copyright © 2018年 watanabe-yudai. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+        
+    }
+    
+    
+    
+}
